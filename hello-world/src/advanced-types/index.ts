7180578d4af0de78eb25b5f20ea2845fbfe3e556
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-04 17:22:35 +0100

    Advanced Types: type intersection

--- hello-world/src/advanced-types/index.ts
+++ hello-world/src/advanced-types/index.ts
@@ -14,14 +14,29 @@ let employee2: Employee = {
 //Union types
 //allows you to give a variable or fn parameter more than one type
 function kgToLbs(weight: number | string): number {
-    //Narrowing is used to narrow down the union type to a specific type
-    if (typeof weight === "number") {
-      return weight * 2.2;
-    } else {
-      return parseInt(weight) * 2.2;
-    }
+  //Narrowing is used to narrow down the union type to a specific type
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
   }
-  
-  kgToLbs(10);
-  kgToLbs("10kg");
-  
+}
+
+kgToLbs(10);
+kgToLbs("10kg");
+
+//Intersection Types
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+  drag: () => console.log("drag"),
+  resize: () => console.log("resize"),
+};
