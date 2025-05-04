0a4773c3f2588d2593fec3a0fb0853e2147f4350
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-04 15:32:30 +0100

    Fundamentals: arrays

--- hello-world/src/fundamentals.ts
+++ hello-world/src/fundamentals.ts
@@ -1,3 +1,4 @@
+//The any Type
 let sales = 123_456_789; //infers type number
 let course = "typescript"; //infers type string
 let isPublished = true; //infers type boolean
@@ -6,3 +7,7 @@ let level; //assumes type any
 function render(document: any) {
   console.log(document);
 }
+
+//Arrays
+let scores: number[] = [1, 2, 3]; //infers type number[]
+scores.forEach((n) => n.toExponential(2)); //offers code completion
