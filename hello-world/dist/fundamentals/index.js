f85832b4333f601a7bee0fef700d2cc46614ed51
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-04 16:16:50 +0100

    Fundamentals: Enums

---
+++ hello-world/dist/fundamentals/index.js
@@ -0,0 +1,19 @@
+"use strict";
+let sales = 123456789;
+let course = "typescript";
+let isPublished = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let scores = [1, 2, 3];
+scores.forEach((n) => n.toExponential(2));
+let user = [1, "Ejike"];
+user[1].length;
+user.push(1);
+const small = 1;
+const meduim = 2;
+const large = 3;
+let mySize = 2;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ hello-world/src/fundamentals/index.ts
@@ -0,0 +1,31 @@
+//The any Type
+let sales = 123_456_789; //infers type number
+let course = "typescript"; //infers type string
+let isPublished = true; //infers type boolean
+let level; //assumes type any
+
+function render(document: any) {
+  console.log(document);
+}
+
+//Arrays
+let scores: number[] = [1, 2, 3]; //infers type number[]
+scores.forEach((n) => n.toExponential(2)); //offers code completion
+
+//Tuples
+let user: [number, string] = [1, "Ejike"];
+user[1].length; //offers code completion
+user.push(1); //doesn't throw errors
+
+//Enums
+const small = 1;
+const meduim = 2;
+const large = 3;
+//it represents a group of related constants
+const enum Size {
+  Small = 1,
+  Meduim,
+  Large,
+}
+let mySize: Size = Size.Meduim;
+console.log(mySize)
