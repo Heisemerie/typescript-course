e03db498d2dcb7ba1bf02ca2bb7e9535fd0f47ac
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-05 18:29:21 +0100

    OOP: Introduction

---
+++ hello-world/src/oop/index.ts
@@ -0,0 +1,12 @@
+//Introduction to OOP
+//Classes
+//Constructors
+//Properties and Methods
+//Access control keywords
+//Getters and Setters
+//Static members
+//Index signatures
+//Inheritance
+//Polymorphism
+//Abstract classes
+//Interfaces
