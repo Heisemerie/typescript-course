6f4b7b661975bc21999e590e9587f7e9ec0825ba
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-06 12:19:58 +0100

    OOP: What is OOP

--- hello-world/src/oop/index.ts
+++ hello-world/src/oop/index.ts
@@ -5,8 +5,15 @@
 //Access control keywords
 //Getters and Setters
 //Static members
-//Index signatures
-//Inheritance
+//Index signatures (creating dynamic properties)
+//Inheritance (technique for reusing code)
 //Polymorphism
 //Abstract classes
 //Interfaces
+
+//OOP
+//It is a programming paradigm. JS supports functional and OOP techniques
+//In OOP objects are the building blocks of the application
+//An object is a unit that contains Data (state) and Operations (behavior)
+//For example you can have a person object with data 'name' and 'email' and operations 'talk()' and 'dance()'
+//They are referred to as properties and methods respectfully
