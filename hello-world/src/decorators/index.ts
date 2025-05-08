07c7d85b5a3d78b57ba912ca8ac5f765e0b21647
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-08 14:24:04 +0100

    Decorators: what are decorators

--- hello-world/src/decorators/index.ts
+++ hello-world/src/decorators/index.ts
@@ -5,3 +5,10 @@
 //Property decorators
 //Accessor decorators
 //Parameter decorators
+
+//What are decorators?
+//Decorators are attributes you can add to classes and their members to change how they behave
+//TS does not have any built-in decorators
+//Under the hood decorators are functions that get called by the JS runtime, it passes the class to the function
+//In that function we can modify the class; we can add new properties, methods or change the implementation of existing methods
+//To create decorators, we have to enable a special compiler feature; "experimentalDecorators"
