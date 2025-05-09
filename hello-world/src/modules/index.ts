7c4c450f862e06994668a611b626421a5a591ebc
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-09 12:37:11 +0100

    Modules: introduction

---
+++ hello-world/src/modules/index.ts
@@ -0,0 +1,6 @@
+//Introduction
+//Creating and using modules
+//Module formats (how to configure the TSC to generate code for different formats)
+//Default exports
+//Wildcard imports
+//Re-exporting 
