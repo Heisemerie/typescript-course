f9efd91947be9f7bc10281a22fb04fbeeecbc997
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-05-09 13:39:41 +0100

    Modules: exporting and importing

--- hello-world/src/modules/index.ts
+++ hello-world/src/modules/index.ts
@@ -3,4 +3,14 @@
 //Module formats (how to configure the TSC to generate code for different formats)
 //Default exports
 //Wildcard imports
-//Re-exporting 
+//Re-exporting
+
+//Exporting and Importing
+//so far we've written all the code in just one file
+//to ease maintenance we split our code into separate files or modules each serving a purpose
+//we move classes 'Circles' and 'Squares' into a new file called 'shapes.ts'
+//then we export them from the module and import them here
+import { Circles as Circle } from "./shapes"; //while importing we can optionally rename it using the 'as' keyword
+
+let circle = new Circle(1);
+console.log(circle.radius);
---
+++ hello-world/src/modules/shapes.ts
@@ -0,0 +1,7 @@
+export class Circles {
+  constructor(public radius: number) {}
+}
+
+export class Squares {
+  constructor(public width: number) {}
+}
