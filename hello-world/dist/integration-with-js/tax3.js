d33eef6eed76d43e0061db9c4a09851bfaf52989
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-12-19 10:53:25 +0100

    refactor: change output directory

--- hello-world/dist/integration-with-js/tax3.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateTax3 = calculateTax3;
-function calculateTax3(income) {
-    return income * 0.3;
-}
-//# sourceMappingURL=tax3.js.map
--- hello-world/dist/modules/cars/Volvo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Volvo = void 0;
-class Volvo {
-}
-exports.Volvo = Volvo;
-//# sourceMappingURL=Volvo.js.map
