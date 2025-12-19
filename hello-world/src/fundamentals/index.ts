e96385b70e53bbedbc13631317b702c2a190d216
Chiemerie Ejike Mbah <ejike162@gmail.com>
2025-12-19 11:56:40 +0100

    fix: rename sectionc correctly

--- hello-world/src/fundamentals/index.ts
+++ hello-world/src/fundamentals/index.ts
@@ -5,12 +5,13 @@
 //Functions
 //Objects
 
-//The any Type
+//Built-in Types
 let sales = 123_456_789; //infers type number
 let course = "typescript"; //infers type string
 let isPublished = true; //infers type boolean
 let level; //assumes type any
 
+//The any Type
 function render(document: any) {
   console.log(document);
 }
